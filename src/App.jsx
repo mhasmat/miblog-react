c44fcd5d96ec79843347aab0e79822356d73d81b
mhasmat <mhasmat@gmail.com>
2024-09-05 19:19:33 -0300

    1er upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Posteos from './components/Posteos'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Mi Blog React</h1>
+      <Posteos />
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/data.js
@@ -0,0 +1,37 @@
+[
+    {
+        "id": 12,
+        "titulo": "Spiderman",
+        "texto": "Joven héroe con poderes arácnidos que combate el crimen balanceándose por Nueva York.",
+        "user_id": 7,
+        "usuario": "stan_lee"
+    },
+    {
+        "id": 14,
+        "titulo": "X-MEN",
+        "texto": "Equipo de mutantes con habilidades únicas que luchan por la coexistencia pacífica entre humanos y mutantes.",
+        "user_id": 7,
+        "usuario": "stan_lee"
+    },
+    {
+        "id": 16,
+        "titulo": "Capitán América",
+        "texto": "Soldado mejorado con un escudo indestructible que lidera a los Vengadores en la defensa del mundo.",
+        "user_id": 7,
+        "usuario": "stan_lee"
+    },
+    {
+        "id": 17,
+        "titulo": "Thanos",
+        "texto": "Titán obsesionado con el poder y las Gemas del Infinito, busca la aniquilación para equilibrar el universo.",
+        "user_id": 7,
+        "usuario": "stan_lee"
+    },
+        {
+        "id": 18,
+        "titulo": "Ant-Man",
+        "texto": "Héroe capaz de encogerse y controlar insectos con un traje especial para combatir el mal.",
+        "user_id": 7,
+        "usuario": "stan_lee"
+    },
+]
